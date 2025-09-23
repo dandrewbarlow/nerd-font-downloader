89788e4feabb6006825ef58cbaa311a3cb97a2e1
Andrew Barlow <dandrewbarlow@gmail.com>
2025-09-22 21:15:17 -0500

    Finally working
    Working version, some adjustments to method and code readability. May
    adjust in future but this is a good minimum viable program.

--- README.md
+++ README.md
@@ -4,18 +4,37 @@ by Andrew Barlow ([github](https://github.com/dandrewbarlow) | [website](https:/
 
 ## Description
 
-I love nerd-fonts for the terminal, I don't love manually installing them. So I
-did the classic move of spending way too much time automating it, but it's a
-learning experience and hopefully other people can enjoy the fruits of my labor
-as well.
+I love nerd-fonts for the terminal, I don't love manually installing them. They
+have a script on their website (which I didn't realize until I finished), but
+I'd rather have my own in my dotfiles, doing exactly what I want it to.
 
 Its a pretty simple shell script, I tried my best to keep things readable. Only
 arcane parts are the `jq` filters, imo. Obviously please try to read and
-understand what it does before running.
+understand what it does before running. 
+
+## Usage
+
+I didn't put in a lot of error checking, and after fiddling with it, only
+decided to have it download one font at a time. So intended use is to run, pick
+font, let it install for each font you want. I leaned heavy on confirmations to
+ensure it does exactly what I want it to and is transparent to the user. Focus
+is on interactivity and ease of use over a properly unix-like program. I only
+want one or two, so more than this is outside of my use case. It's a simple
+enough program that I feel like someone who cares about that could adjust it
+themselves.
+
+### Configuration
+
+I have any relevant variables (download/install locations) defined at the
+beginning of the file, I think they're sensible defaults, but if you disagree
+or have a different setup it should be very easy to change. Last disclaimer,
+this was made on and for a (/my) Linux environment, it could probably be easily
+made to work elsewhere, but it's not my priority.
 
 ## Dependencies
 
 - `fzf`
 - `jq`
 - `wget`
-- `parallel`
+- `tar`
+- `unzip`
--- nfd.sh
+++ nfd.sh
@@ -13,112 +13,206 @@
 # fzf
 # jq
 # wget
-# parallel
+# tar
+# unzip
 
 
 # GLOBAL VARS ##################################################
+# NOTE: keeping variables you may want to change here
 
 # By default, save files needed in xdg-cache location
 cache_dir="$XDG_CACHE_HOME/nerd-font-downloader"
 
-# if xdg-cache location isn't defined, default to ~/.config/nerd-font-downloader
+# if xdg-cache location isn't defined, default to ~/.cache/nerd-font-downloader
 if [ -z "$XDG_CACHE_HOME" ]; then
 	cache_dir="$HOME/.cache/nerd-font-downloader"
 fi
-
-# could be hardcoded, but I might refactor this script for more general usage
-author="ryanoasis"
-repo="nerd-fonts"
-
-# location of font list in github repo. technically not necessary, could just
-# use releases, but this has extra info on fonts that could be nice to see.
-font_list_file="bin/scripts/lib/fonts.json"
 
 # names for json files saved to cache directory
 registry_cache_file="$cache_dir/registry.json"
 release_cache_file="$cache_dir/release.json"
 
+# font download location
+font_download_dir="/tmp/nerd-font-downloader"
+
+# NOTE: defaults to user installation; for system-wide installation, change
+# this variable to /usr/local/share/fonts
+font_install_location="$HOME/.local/share/fonts"
+
+# Script variables: changing these will break script
+
+# could be hardcoded, but I might refactor this script for more general usage
+author="ryanoasis"
+repo="nerd-fonts"
+
+# NOTE: location of font list in github repo. technically not necessary, could
+# just use releases, but this has extra info on fonts that could be nice to
+# see.
+font_list_file="bin/scripts/lib/fonts.json"
+
 # FUNCTIONS ##################################################
 
-# get font registry
+# handles actual downloading and parsing of font registry
+fetch_font_registry() {
+		
+	# download, extract & decode json file, filter out irrelavent data
+	# TODO: error handling
+	curl -s -L "https://api.github.com/repos/$author/$repo/contents/$font_list_file" | \
+		jq -r '.content' | base64 --decode | \
+		jq -r -c '[.fonts[] | {name: .patchedName, version: .version, isMonospaced: .isMonospaced, description: .description}]'
+}
+
+# wrapper to check if font registry exists and download if not
 update_font_registry() {
 
 	# TODO: this only checks if it exists; a nice feature could be to check
 	# for updates or update after a certain amount of time; rn have to
 	# remove this file to get it to refresh
 	if [ ! -e "$registry_cache_file" ]; then
-
 		echo "Downloading Font Registry"
-		
-		# download, extract & decode json file, filter out irrelavent data
-		curl -s -L "https://api.github.com/repos/$author/$repo/contents/$font_list_file" | \
-			jq -r '.content' | base64 --decode | \
-			jq -r '[.fonts[] | {name: .patchedName, version: .version, isMonospaced: .isMonospaced, description: .description}]' \
-			> "$registry_cache_file"
+		fetch_font_registry > "$registry_cache_file"
 	fi
 }
 
+# downloads releases from github and parses out relevant data before saving
+fetch_releases() {
 
-# get latest release info
+	# download and filter out irrelavent data
+	# jq gets a little complicated
+	# TODO: error handling
+	# TODO: wget has -N flag which only downloads if file on server is newer, better option for this
+	
+	# NOTE: this creates seperate entries for .zip and .tar.xz downloads.
+	# I've make the executive decision that the added space is trivial
+	# compared to figuring out how to merge them with jq
+	curl -s -L "https://api.github.com/repos/$author/$repo/releases/latest" | \
+		jq -c '[.assets[] | {name: .name | sub("(.tar.xz|.zip)"; ""), url: [.browser_download_url]}]'
+}
+
+# check if releases cache exists and downloads it if not
 update_releases() {
 
 	# TODO: same as registry, this only checks for file existance
 	if [ ! -e "$release_cache_file" ]; then
-
 		echo "Downloading Release Registry"
-
-		# download and filter out irrelavent data
-		# jq gets a little complicated
-		curl -s -L "https://api.github.com/repos/$author/$repo/releases/latest" | \
-			jq '[.assets[] | {name: .name|rtrimstr(".tar.xz"), url: .browser_download_url}] | del(.[] | select(.name | test(".zip", "g")))' > \
-			"$release_cache_file"
+		fetch_releases > "$release_cache_file"
 	fi
 }
 
-
 # pick which fonts to download
 select_font() {
-	cat "$registry_cache_file" | jq -r '.[].name' | fzf --prompt="Choose which fonts to download> "
+	cat "$registry_cache_file" | jq -r '.[].name' | fzf +m --prompt="Choose which fonts to download> "
 }
 
-# get font urls
-extract_urls() {
-	# BUG: creates seperate entries for .zip & .tar.xz; wanted to just use
-	# tar for smaller size, but not all fonts have a tarball 
-	for var in "$@"; do
-		jq -r --arg fontname "$var" 'map(select(.name == $fontname)) | .[0].url' "$release_cache_file" | fzf --prompt="Confirm downloads for font: $var"
-	done
+# get font json for a given font
+get_font_info() {
+	# using some complicated jq to merge url fields
+	jq -r --arg fontname "$1" 'map(select(.name == $fontname)) | [{name: .[].name, urls: [.[].url | add]}][0]' "$release_cache_file"
 }
 
-# TODO: refactor for function; remove all 'null's from url list
-verify_url() {
+# extract & confirm which download to get
+confirm_download() {
+	font_name=$(echo "$1" | jq '.name')
+
+	urls="$(echo "$1" | jq -r '.urls[]')"
+
 	# get line numbers of releases not found
-	echo "$urls" | grep -n 'null' | cut -d : -f 1 | xargs -I _ echo "${fonts[_]}"
+	downloadURL=$(echo "$urls" | fzf +m --prompt="Choose download for font: $font_name")
+	# echo "$urls" | grep -n 'null' | cut -d : -f 1 | xargs -I _ echo "${fonts[_]}"
+	#
+	echo "$downloadURL"
 }
 
-# download fonts in parallel
-download_fonts() {
-	# TODO: set download location to something sensible 
-	echo "$@" | grep -v 'null' | parallel wget -q
+# get download location based on font name making a function because I need
+# this at a couple of different points and don't want to bother passing it
+# around
+get_extraction_location() {
+	echo "$font_download_dir/$1"
 }
 
-# TODO: extract fonts
-extract_fonts() {}
+# high level function to download a font
+download_font() {
+	font="$1"
 
-# TODO: install downloaded fonts
-install_fonts() {}
+	# gets release info including download urls
+	font_meta=$(get_font_info "$font")
+
+	# gets the url of the file the user wants to download
+	url=$(confirm_download "$font_meta")
+
+	# provide some feedback
+	echo "Downloading $font"
+	echo "=================================================="
+
+	# download the archive
+	echo "$url" | wget -nv -N -i - -P "$font_download_dir"
+}
+
+# function to extract downloaded font archive
+extract_font() {
+
+	font="$1"
+
+	font_extraction_location=$(get_extraction_location "$font")
+
+	mkdir -p "$font_extraction_location"
+
+	echo "Extracting $font"
+	echo "=================================================="
+
+	# handle .tar.xz archives
+	find "$font_download_dir" -name "$1.tar.xz" | xargs -p -I {} tar xvf {} -C "$font_extraction_location"
+
+	# handle .zip archives
+	find "$font_download_dir" -name "$1.zip" | xargs -p -I {} unzip {} -d "$font_extraction_location"
+}
+
+# high level function for installing a font
+install_font() {
+	font="$1"
+
+	font_extraction_location=$(get_extraction_location "$font")
+
+	echo 
+	read -p "Installing $font to $font_install_location  continue? [y/n]: " -n 1 -r
+	echo    # (optional) move to a new line
+
+	if [[ $REPLY =~ ^[Yy]$ ]]
+	then
+		# move font files to installation location
+		mv "$font_extraction_location" "$font_install_location"
+
+		# make system update its list of fonts
+		echo
+		echo "Refreshing fontconfig cache"
+		fc-cache
+	fi
+
+}
 
 # initialization
 init() {
-	mkdir -p "$cache_dir"
+	# create necessary directories for download
+	mkdir -p "$cache_dir" "$font_download_dir"
+
+	# check if there are newer releases on the github
+	# I'm an arch user btw, can't miss any font updates
 	update_font_registry
 	update_releases
 }
 
+# main process, made it as high level as possible
 main() {
+
 	init
-	fonts="$(select_font)"
-	urls="$(extract_urls $fonts)"
+
+	font="$(select_font)"
+
+	download_font "$font"
+
+	extract_font "$font"
+
+	install_font "$font"
 }
 
 
